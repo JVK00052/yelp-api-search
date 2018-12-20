ae10b8e2cb8d371ba8b603ae5e9cd4268b16e1fb
unknown <JVK00052@gmail.com>
2018-12-20 13:58:23 -0500

    intial finished commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ApiService } from '../app/services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'YelpApi';
+  title = 'Yelp API Search';
+  yelpForm = new FormGroup({
+  price: new FormControl(),
+  city: new FormControl(),
+  })
+  restaurants: any;
+
+  constructor(private _AS: ApiService) { }
+  delivery: string;
+
+  getData() {
+    this._AS.getData(this.yelpForm.value.city, this.yelpForm.value.price).subscribe(res => {
+    this.restaurants = res.businesses[Math.floor(Math.random()*res.businesses.length)]
+    console.log(this.restaurants);
+    let delivery = this.restaurants.transactions.indexOf('delivery')
+    if (delivery > 0) {
+      this.delivery = "Delivery and Pickup"
+    } else {
+      this.delivery = "Delivery and Dine-In Only"
+    }
+    return this.restaurants
+    })
+  }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  APIKEY: 'Bearer 4fUzs5J5lVvnlXPiLWjN6X5a7LpABk_Dh2JJhBlENHrrOvK-t8EYgXHv4EAMa1KqqJjC-tXP0pyy_4sel5GoMWpaSDwlz42qcT-ifcx677V1s-UfjfGJMuaEqLgaXHYx',
   production: true
 };
